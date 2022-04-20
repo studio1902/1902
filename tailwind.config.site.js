c9c95fe67860f1050bf6dcdd678d93e44defcbc5
Rob de Kort <rob@studio1902.nl>
2022-04-20 14:33:00 +0200

    Update responsive sizes

--- tailwind.config.site.js
+++ tailwind.config.site.js
@@ -48,6 +48,7 @@ module.exports = {
         },
         up: {
           '0%': { transform: 'translateY(0)', opacity: 1 },
+          '75%': { opacity: 0 },
           '100%': { transform: 'translateY(-100%)', opacity: 0 },
         }
       },
