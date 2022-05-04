3df4300c887dde1d0e5b7c9cee5f225bd5dcdeee
Rob de Kort <rob@studio1902.nl>
2022-05-04 13:57:12 +0200

    Header bg blur and z-index

--- content/collections/pages/nl/home.md
+++ content/collections/pages/nl/home.md
@@ -3,7 +3,7 @@ id: home
 blueprint: page
 title: Home
 updated_by: b40458fa-e73a-4f88-bfa1-570cd54b72e0
-updated_at: 1651583973
+updated_at: 1651665313
 seo_noindex: false
 seo_nofollow: false
 seo_canonical_type: entry
@@ -11,7 +11,7 @@ sitemap_change_frequency: weekly
 sitemap_priority: 0.5
 page_builder:
   -
-    title: "Studio 1902\L is het huis van Rob de Kort, Statamic developer."
+    title: "Studio 1902 is het huis van Rob de Kort, Statamic developer."
     type: title_and_text
     enabled: true
     introduction:
--- tailwind.config.peak.js
+++ tailwind.config.peak.js
@@ -197,5 +197,22 @@ module.exports = {
       }
       addUtilities(newUtilities)
     }),
+
+    // Target firefox only.
+    plugin(function ({ addVariant, e, postcss }) {
+      addVariant("firefox", ({ container, separator }) => {
+        const isFirefoxRule = postcss.atRule({
+          name: "-moz-document",
+          params: "url-prefix()",
+        });
+        isFirefoxRule.append(container.nodes);
+        container.append(isFirefoxRule);
+        isFirefoxRule.walkRules((rule) => {
+          rule.selector = `.${e(
+            `firefox${separator}${rule.selector.slice(1)}`
+          )}`;
+        });
+      });
+    }),
   ]
 }
