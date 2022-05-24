865bf7fcd513c61eaf0649064359a5c02e3137a8
Rob de Kort <rob@studio1902.nl>
2022-05-24 11:22:54 +0200

    Fix html syntax highlighting

--- resources/views/components/_torchlight.blade.php
+++ resources/views/components/_torchlight.blade.php
@@ -1,1 +1,11 @@
-<pre><x-torchlight-code :language="$mode == 'handlebars' ? 'antlers' : $mode">{!! $code !!}</x-torchlight-code></pre>
+@switch($mode)
+    @case('handlebars')
+        @php $language = 'antlers' @endphp
+        @break
+    @case('htmlmixed')
+        @php $language = 'html' @endphp
+        @break
+    @default
+        @php $language = $mode @endphp
+@endswitch
+<pre><x-torchlight-code :language="$language">{!! $code !!}</x-torchlight-code></pre>
